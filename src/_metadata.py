207842339bc8f537d96ff21c7ec21b01114a2278
jbloom <jbloom@fhcrc.org>
2016-01-23 14:55:58 -0800

    updated author to all contributors on GitHub

--- src/_metadata.py
+++ src/_metadata.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.11'
-__author__ = 'Jesse D. Bloom'
+__version__ = '1.1.dev12'
+__author__ = 'the Bloom Lab (see https://github.com/jbloom/dms_tools/graphs/contributors for all contributors)'
 __url__ = 'http://jbloom.github.io/dms_tools/'
 __author_email__ = 'jbloom@fredhutch.org'
