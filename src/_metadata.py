a8f1d3ad955aebb0394dfcc421c0ccdb4916126a
jbloom <jbloom@fhcrc.org>
2016-02-16 20:57:00 -0800

    Updated version number to 1.1.12

--- src/_metadata.py
+++ src/_metadata.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.dev12'
+__version__ = '1.1.12'
 __author__ = 'the Bloom Lab (see https://github.com/jbloom/dms_tools/graphs/contributors for all contributors)'
 __url__ = 'http://jbloom.github.io/dms_tools/'
 __author_email__ = 'jbloom@fredhutch.org'
